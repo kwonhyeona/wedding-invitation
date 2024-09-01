bebfd21d695c45903bec41305e64bbb7006b99f0
kwonhyeona <gusdk7656@naver.com>
2024-09-02 03:26:55 +0900

    feat: safe-area 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import brotherSrc from '@/assets/thanks/IMG_2248.png';
 
 const Footer = () => {
   return (
-    <div className="bg-lime-600 text-white w-full flex flex-col items-center justify-center py-8">
+    <div className="bg-lime-600 text-white w-full flex flex-col items-center justify-center py-8 pb-[calc(2rem+env(safe-area-inset-bottom))]">
       <p className="font-deluxeEdition text-3xl mb-4">Special Thanks To</p>
       <div className="w-full flex flex-row justify-evenly items-center px-8">
         <div className="flex flex-col items-center justify-center gap-2">
