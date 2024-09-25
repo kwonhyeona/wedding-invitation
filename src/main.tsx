ec2af282243a0de5d518538d2b3afb4d7f2c7f43
kwonhyeona <gusdk7656@naver.com>
2024-09-25 23:24:11 +0900

    feat: vercel speed insights 추가

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import App from '@/App';
 import dayjs from 'dayjs';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import titleSrc from '@/assets/title.png';
 
@@ -20,6 +21,7 @@ image.onload = () => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
+      <SpeedInsights />
     </React.StrictMode>
   );
 };
